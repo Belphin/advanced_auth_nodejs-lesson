4b956397b0cfe8aa26339cee91af9a7eee5de450
Belphin <findolph0003@gmail.com>
2023-02-22 20:12:33 +0200

    registration

--- server/index.js
+++ server/index.js
@@ -9,8 +9,9 @@ const PORT = process.env.PORT || 5000;
 const DB_URL = process.env.DB_URL;
 
 const app = express();
-app.use(cookieParser());
+app.use(express.json());
 app.use(cors());
+app.use(cookieParser());
 app.use("/api", router);
 
 const start = async () => {
