6a06871924a4cb23c13918b4166e8dfa26832a52
Belphin <findolph0003@gmail.com>
2023-02-27 16:01:53 +0200

    server auth

--- server/controllers/user.controller.js
+++ server/controllers/user.controller.js
@@ -37,6 +37,10 @@ class UserController {
 
 	async logout(req, res, next) {
 		try {
+			const { refreshToken } = req.cookies;
+			const token = await userService.logout(refreshToken);
+			res.clearCookie("refreshToken");
+			return res.json({ message: "Logout completed successfully" });
 		} catch (e) {
 			next(e);
 		}
@@ -54,6 +58,13 @@ class UserController {
 
 	async refresh(req, res, next) {
 		try {
+			const { refreshToken } = req.cookies;
+			const userData = await userService.refresh(refreshToken);
+			res.cookie("refreshToken", userData.refreshToken, {
+				maxAge: 30 * 24 * 60 * 60 * 1000,
+				httpOnly: true,
+			});
+			return res.json(userData);
 		} catch (e) {
 			next(e);
 		}
@@ -61,6 +72,8 @@ class UserController {
 
 	async getUsers(req, res, next) {
 		try {
+			const users = await userService.getAllUsers();
+			return res.json(users);
 		} catch (e) {
 			next(e);
 		}
